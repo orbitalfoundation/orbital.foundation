923a3a34a151fdd6d78417925892cc48df9b6733
Anselm Hook <anselm@gmail.com>
2023-04-27 19:02:30 -0700

    revised orbital site

--- .lifecards.js
+++ .lifecards.js
@@ -1,36 +1,65 @@
-// banner ⚪ Orbital Docs
 
-let one = {
+let css = {
 content:
 `
 <style>
-.callout {
-	display: flex;
-	justify-content: center;
-	margin: auto;
-	flex-direction: row-reverse;
-	max-width: 70%;
+body {
 	font-family: 'Poppins', sans-serif;
+	xfont-size:  2vw;
+	line-height: 2em;
+	margin: 0px;
+	padding: 0px;
 }
-.callout div {
-	flex: 1 1 0;
+h1 {
+	font-family: 'Poppins', sans-serif;
+	font-weight:  400;
+	line-height:  1.25em;
+	font-size: 3.0em;
 }
-.callout div:first-child {
-	flex: 1 1 0;
+h2 {
+	font-family: 'Poppins', sans-serif;
+	font-weight:  300;
+	line-height:  1.25em;
+}
+
+centerer {
+	display: block;
+	margin: auto;
+	width: 70vw;
+}
+
+callout {
 	display: flex;
-	justify-content: center;
+	align-items: center;
+	flex-direction: row;
+	font-family: 'Poppins', sans-serif;
+	gap: 5%;
+}
+
+.forceabove {
+
+}
+
+callout div {
+	flex: 1 1 0;
+}
+
+callout img {
 	background-size: contain;
 	background-repeat: no-repeat;
 	background-position: center center;
 	background-image: url(/assets/15flipped.png);
+	object-fit: contain;
+	max-width: 50vw;
+	width:50%;
 }
 
 button {
 	font-family: 'Poppins', sans-serif;
 	font-size: 0.95em;
-	border-radius: 52px;
+	border-radius: 5px;
 	padding: 10px 30px 10px 30px;
-	margin-right: 10px;
+	margin: 10px 10px 0 0;
 	font-size:  20px;
 	font-weight:  300;
 	background: transparent;
@@ -45,8 +74,11 @@ button:hover {
 }
 
 @media only screen and (max-width: 800px) {
-	.callout {
+	callout {
 		flex-direction: column;
+	}
+	.forceabove {
+		flex-direction: column-reverse;
 	}
 }
 
@@ -61,161 +93,155 @@ button:hover {
 		transition: 0.3s;
 	}
 }
-
-
 </style>
-<div class="callout">
-<div>
-	<img style="width:100%; object-fit: contain;" src="/assets/25.png"></img>
-</div>
-<div>
-	<h1>Orbital is an open source platform for distributed computation</h1>
-	<p>Create interactive digital twins, simulations, games, AR experiences, and open metaverses.</p>
-	<button>Get<br/>Started</button>
-	<button>Learn<br/>More</button>
-</div>
-</div>
 `
 }
 
-//////////////// ignore
+let one = {
+content:
+`
+<centerer>
+
+<br/>
+<div style="display:flex;justify-content:space-between">
+<a href="/" style="font-size:1.4em">⚪ Orbital Foundation</a>
+<span style="flex-basis: auto"></span>
+<span style="flex-basis: auto"></span>
+<span style="flex-basis: auto"></span>
+<a href="https://docs.orbital.foundation">Docs</a>
+<a href="https://github.com/orbitalfoundation"><img src="/assets/github.png" style="width:32px;background:white;border-radius:32px"/></a>
+</div>
+<br/>
+
+<callout class="forceabove">
+	<div>
+		<h1>Orbital is an open source platform for distributed computation</h1>
+		<p>Create interactive digital twins, simulations, games, AR experiences, and open metaverses.</p>
+		<button>Get<br/>Started</button>
+		<button>Learn<br/>More</button>
+	</div>
+	<img src="/assets/25.png"/>
+</callout>
+</centerer>
+<br/>
+<br/>
+`
+}
 
 let two = {
-	content:
+stylize:{backgroundColor:"rgba(0,0,0,0.1)"},
+content:
 `
-<div style="display:flex">
-	<div>
-		<img style="width:200px;" src="/assets/35flipped.png"></img>
-	</div>
-	<div>
+<centerer>
+<br/>
+<h1>Examples</h1>
+<div style="border:2px solid grey; width: 100%"></div>
+<br/>
+<br/>
+
+<!--
+<callout>
+	<img src="/assets/35flipped.png"/>
+	<div style="">
 		<h2>Modeling Urban Life</h2>
-		People in a community need to find ways to work together at a civic level to contribute to local issues. Although experts have tools like this - can we make these tools accessible to wider audiences. Here we explore a simple car simulation to help understand traffic patterns and possible road-blocks.
-		<br/>
+		<p>People in a community need to find ways to work together to solve local issues. Can we make these tools accessible to all stakeholders? Here we explore a simple car simulation to help understand traffic patterns and possible road-blocks.</p>
 		<button>Visit Car World</button>
 	</div>
-</div>
-`
-,
-}
+</callout>
+<br/>
+<br/>
 
-let three = {
-	content:
-`
-<div style="display:flex">
+<callout>
+	<img src="/assets/51.png"/>
 	<div>
 		<h2>Deep Learning Modules</h2>
-		How can we provide access to new machine learning tools? Can we make it easier for people to leverage tensorflow or newer emerging techologies? This example explores scripting a lightweight app that uses computer vision.
-		<br/>
-		<button>Visit Computer Vision</button>
+		<p>Can non technical people take advantage of AI and newer emerging techologies? This example explores scripting a lightweight app that uses computer vision.</p>
+		<button>Run CV Demo</button>
 	</div>
-	<div>
-		<img style="width:200px;" src="/assets/51.png"></img>
-	</div>
-<div>
-`
-,
-}
+</callout>
+<br/>
+<br/>
 
-let four = {
-	content:
-`
-<div style="display:flex">
+<callout>
+	<img src="/assets/51.png"/>
 	<div>
-		<img style="width:200px;" src="/assets/59.png"></img>
+		<h2>Deep Learning Modules</h2>
+		<p>Can non technical people take advantage of AI and newer emerging techologies? This example explores scripting a lightweight app that uses computer vision.</p>
+		<button>Enter Virtual Venice</button>
 	</div>
-	<div>
-		<h2>Virtual Venice</h2>
-		Digital Twins combine real and virtual information, populating the digital model with sensor data – ideally allowing participants in the virtual model to impact the real world. This is an example of a 'digital twin' of Venice, Italy.
-		<br/>
-		<button>Visit Virtual Venice</button>
-	</div>
-</div>
-`,
-}
+</callout>
+<br/>
+<br/>
+-->
 
-let five = {
-	content:
-`
-<div style="display:flex">
+<callout>
+	<img src="/assets/36.png"/>
 	<div>
-		<h2>Natural Systems</h2>
-			There's a possibility of civic engagement around decision-making if we can have a shared understanding. In this case, we see digital simulations running in the cloud as a way to do this. This example explores coral reef ecologies, systems that efficiently capture the energy and act as single organisms with emergent behaviors. Reefs are impacted by global warming, and it may be possible to use digital tools to predict the outcome of policy changes here.
-		<br/>
+		<h2>Augmented Ecosystems</h2>
+		<p>Can we use a combination of human and machine reasoning to understand problems that cross many boundaries?
+			In this example we explore a coral reef to examine human and natural impacts.</p>
 		<button>Visit Cloud Reef</button>
 	</div>
-	<div>
-		<img style="width:200px;" src="/assets/36.png"></img>
-	</div>
-</div>
+</callout>
+
+</centerer>
+<br/>
+<br/>
 `	
 }
 
-let six = {
+let three = {
 content:
 `
+<centerer>
 <h1>Orbital – A blue-sky reinterpretation of the personal computing</h1>
-`,
-children:[
-	{
-		content:`
-		<h1>Personal Agency</h1>
-		We see a future of “human scale” software where everybody can create and share lightweight interactive apps as easily as we share static images, text and video today. Orbital will make it easy for a farmer to describe a new deep learning leaf recognition algorithm to their farmbot. To allow a child to create and share a pet recognizer. A fitness trainer to create an interactive workout. An artist to create a vignette of the experience of walking beneath cherry blossoms in the spring.
-		`
-	},
-	{
-		content:`
-		<h1>Sense Making</h1>
-		Today millions of us rely on software agents to help us navigate and understand our increasingly complex world. Emerging technologies such as deep learning algorithms, computer vision, augmented reality glasses and many other new innovations are useful, but only they can meet our subjective needs. Orbital will make new technologies dynamically loadable such that professional developers and novices can define services on demand.
-
-		`
-	},
-	{
-		content:`
-		<h1>Digital Agents</h1>
-		We envision software apps, services, libraries, databases and agents as being defined by standardized portable components, able to interoperate, able to be wired together easily and able to run on consumer devices or migrate over the cloud as needed. Apps will be composed of shared services and run persistently in the background or in a distributed computational soup.
-		`
-	},
-	{
-		content:`
-		<h1>New Horizons</h1>
-		Humanity faces many grand challenges from global warming to unrestricted AI. We are communities as much if not more so than individuals. A synthesis of human and machine reasoning can help us 'see' better; to model near term collective outcomes and to navigate these issues together. We see Orbital as a platform that allows us to leverage our tools, build on each others capabilities and to face the future.
-		`
-	},
-]
-
+<div style="border:2px solid grey; width: 100%"></div>
+<br/>
+<h2>Personal Agency</h2>
+We see a future of “human scale” software where everybody can create and share lightweight interactive apps as easily as we share static images, text and video today. Orbital will make it easy for a farmer to describe a new deep learning leaf recognition algorithm to their farmbot. To allow a child to create and share a pet recognizer. A fitness trainer to create an interactive workout. An artist to create a vignette of the experience of walking beneath cherry blossoms in the spring.
+<h2>Sense Making</h2>
+Today millions of us rely on software agents to help us navigate and understand our increasingly complex world. Emerging technologies such as deep learning algorithms, computer vision, augmented reality glasses and many other new innovations are useful, but only they can meet our subjective needs. Orbital will make new technologies dynamically loadable such that professional developers and novices can define services on demand.
+<h2>Digital Agents</h2>
+We envision software apps, services, libraries, databases and agents as being defined by standardized portable components, able to interoperate, able to be wired together easily and able to run on consumer devices or migrate over the cloud as needed. Apps will be composed of shared services and run persistently in the background or in a distributed computational soup.
+<h2>New Horizons</h2>
+Humanity faces many grand challenges from global warming to unrestricted AI. We are communities as much if not more so than individuals. A synthesis of human and machine reasoning can help us 'see' better; to model near term collective outcomes and to navigate these issues together. We see Orbital as a platform that allows us to leverage our tools, build on each others capabilities and to face the future.
+</centerer>
+<br/>
+`
 }
 
-// https://devpractical.com/3-columns-flexbox-layout-css/
-let seven = {
-	content:
+let four = {
+stylize:{backgroundColor:"rgba(0,0,0,0.1)"},
+content:
 `
-<div style="display:flex">
+<centerer>
+<hr/>
+<div style="display:flex;justify-content:space-between">
+
 	<div>
-		<h1>Orbital Foundation</h1>
-		<span>twitter</span>
-		<span>github</span>
-		<span>discord</span>
+		<h2>Links</h2>
+		<a href="https://docs.orbital.foundation">Docs</a><br/>
+		<a href="https://ecosystem.orbital.foundation">Ecosystem</a><br/>
+		<a href="https://github.com/orbitalfoundation">Github</a><br/>
 	</div>
+
 	<div>
-		<h1>Links</h1>
-		<h1>Docs</h1>
-		<h1>Ecosystem</h1>
-		<h1>Github</h1>
-	</div>
-	<div>
-		<h1>Resources</h1>
-		<h1>Development</h1>
-		<h1>Code of Conduct</h1>
-		<h1>Apache License</h1>
+		<h2>Other Resources</h2>
+		<a href="https://wiki.orbital.foundation">Development Wiki</a><br/>
+		<!--
+		<a href="https://orbital.foundation/apache-license">Apache Open Source License</a><br/>
+		<a href="https://orbital.foundation/code-of-conduct">Code of Conduct</a><br/>
+		-->
 	</div>
 
 </div>	
+<br/>
 `
 }
 
 let site = {
 	uuid: '/',
-	children: [ one ] // , two, three, four, five, six,seven ]
+	children: [ css, one, two, three, four ]
 }
 
 let data = [
